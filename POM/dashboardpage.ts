1a7783ed58d1f62f1ceb69d310d559d881c2b767
tbalug <tanja.krivacic@gmail.com>
2024-02-27 20:20:59 +0100

    Playwright installed dashboardpage file added testConstants file added

--- POM/dashboardpage.ts
+++ POM/dashboardpage.ts
@@ -1,5 +1,5 @@
 import {expect, type Locator, type Page} from '@playwright/test';
-import { pageLinks, headerText } from './testConstants';
+import { pageLinks, dashboardPageTexts } from './testConstants';
 
 export class DashboardPage {
 
@@ -8,15 +8,33 @@ export class DashboardPage {
     readonly mainHeaderText: Locator;
     readonly welcomeMessage:Locator;
     readonly welcomeMessage2: Locator;
+    readonly apiDocumentationLinkButton: Locator;
     readonly logInTitle: Locator;
     readonly emailPlaceholder: Locator;
-    
+    readonly passwordPlaceholder: Locator;
+    readonly buttonSubmit: Locator;
+    readonly signUpTitle: Locator;
+    readonly signUpButton: Locator;
+    readonly footerMessage: Locator;
+    readonly footerLogo: Locator;
 
 
     constructor (page: Page) {
 
         this.page = page;
-        this.mainHeaderText = page.locator('(//div/main//h3)[1]');
+        this.mainHeaderText = page.locator('//body/h1');
+        this.welcomeMessage = page.locator('');
+        this.welcomeMessage2 = page.locator('');
+        this.apiDocumentationLinkButton = page.locator('');
+        this.logInTitle = page.locator('');
+        this.emailPlaceholder = page.locator('');
+        this.passwordPlaceholder = page.locator('');
+        this.buttonSubmit = page.locator('');
+        this.signUpTitle = page.locator('');
+        this.signUpButton = page.locator('');
+        this.footerMessage = page.locator('');
+        this.footerLogo = page.locator('');
+        
     }
 
     async goToDashboardPage(){
@@ -26,6 +44,6 @@ export class DashboardPage {
 
     async checkHeaderText() {
 
-        await expect(this.mainHeaderText).toHaveText(headerText.mainHeaderText);
+        await expect(this.mainHeaderText).toHaveText(dashboardPageTexts.mainHeaderText);
     }
 }
--- POM/testConstants.ts
+++ POM/testConstants.ts
@@ -1,10 +1,18 @@
 export enum pageLinks {
 
-    dashboardLink = "https://gh-users-search.netlify.app/",
+    dashboardLink = "https://thinking-tester-contact-list.herokuapp.com/",
 }
 
 
-export enum headerText {
+export enum dashboardPageTexts {
 
-    mainHeaderText = "Welcome",
+    mainHeaderText = "Contact List App",
+    welcomeMessage = "Welcome! This application is for testing purposes only. The database will be purged as needed to keep costs down.",
+    welcomeMessage2 = "The API documentation can be found here.",
+    logInTitle = "Log In:",
+    emailPlaceholder = "Email",
+    passwordPlaceholder = "Password",
+    signUpTitle = "Not yet a user? Click here to sign up!",
+    footerMessage = "Created by Kristin Jackvony, Copyright 2021 ",
+    
 }
